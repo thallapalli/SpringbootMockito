593d1b39c2964bf4a1a53f0e02e1a801af71c452
karnakar <karnakar@ymail.com>
2020-07-08 22:14:15 -0400

    Initail checkin

---
+++ src/test/java/com/kt/spring/mockito/SpringbootMockito/SpringbootMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.spring.mockito.SpringbootMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
